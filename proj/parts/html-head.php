2aa833a534c6f17273e312a1aae9e3bb789f2715
Chuan Min <chuanmin13@gmail.com>
2022-06-08 18:04:40 +0800

    add avatar css file

--- proj/parts/html-head.php
+++ proj/parts/html-head.php
@@ -18,8 +18,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../fontawesome-free-6.1.1-web/css/all.min.css">
 
-    <!-- cm-css -->
-    <link rel="stylesheet" href="./css/cm-style.css">
+    <!-- place css -->
+    <link rel="stylesheet" href="./css/place-style.css">
+
+    <!-- avatar css -->
+    <link rel="stylesheet" href="./css/avatar.css">
 </head>
 
 <body>
