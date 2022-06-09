76f9e9266695a3a50ceaaa5bac175d74a0be3848
hsin007 <a2827234@gmail.com>
2022-06-09 10:42:30 +0800

    datafield update for member

--- hsin/ab-member-datafield.php
+++ hsin/ab-member-datafield.php
@@ -1,20 +1,26 @@
 <?php
 
+// member的資料表欄位範例
 $_SESSION = [
     'member' => [
-        'sid' => 605,
-        'name' => '獨行俠',
-        'deathdate' => NULL
+        'sid' => 29,
+        'name' => '快 樂 貓',
+        'birthdate' => '2007-03-03',
+        'deathdate' => '2021-07-12',
+        'mobile' => '0977-777-000',
+        'email' => 'HappyCat07@gmail.com',
+        'account' => 'HappyCat07',
+        'password' => 'HappyCat07'
     ], 
 ];
 
-// 葉宥廷
-$_SESSION['member']['sid'] = 11;
-$_SESSION['member']['deathdate'] = '2022-06-06';
-// $_SESSION['member']['account'] = 'HappyCat03';
-
-// 陳怡雯
-// $_SESSION['member']['sid'] = 12; 
-// $_SESSION['member']['deathdate'] = NULL; 
-
+// 快 樂 貓
+$_SESSION['member']['sid'] = 29;
+$_SESSION['member']['name'] = '快 樂 貓';
+$_SESSION['member']['birthdate'] = '2007-03-03';
+$_SESSION['member']['deathdate'] = '2021-07-12';
+$_SESSION['member']['mobile'] = '0977-777-000';
+$_SESSION['member']['email'] = 'HappyCat07@gmail.com';
+$_SESSION['member']['account'] = 'HappyCat07';
+$_SESSION['member']['password'] = 'HappyCat07';
 ?>
