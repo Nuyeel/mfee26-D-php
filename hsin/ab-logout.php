c2bb75de02124364d7718b8b7b878993710c952f
hsin007 <a2827234@gmail.com>
2022-06-09 14:56:53 +0800

    Logout file update

--- hsin/ab-logout.php
+++ hsin/ab-logout.php
@@ -3,15 +3,12 @@ header('Content-Type: application/json');
 
 // session_start();
 unset($_SESSION['member']['account']);
-<<<<<<< HEAD
 unset($_SESSION['member']['sid']);
 unset($_SESSION['member']['name']);
 unset($_SESSION['member']['birthdate']);
 unset($_SESSION['member']['deathdate']);
 unset($_SESSION['member']['mobile']);
 unset($_SESSION['member']['email']);
-=======
->>>>>>> 970e3217017620593ad040be40e5f8f30c85198a
 header('location:ab-login.php');
 
 // if (isset($_POST['logout']) && $_POST['logout'] == "true") {
@@ -19,11 +16,6 @@ header('location:ab-login.php');
 //     header('location:ab-login.php');
 // } 
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 970e3217017620593ad040be40e5f8f30c85198a
 // $output['postData'] = $_SESSION['member']['account'];
 // echo json_encode($output, JSON_UNESCAPED_UNICODE);
 // exit;
