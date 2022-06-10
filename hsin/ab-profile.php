57e7081756af16e5296b64cafad7f8ac6919ce30
hsin007 <a2827234@gmail.com>
2022-06-10 16:01:50 +0800

    adjust CSS style for member profile

--- hsin/ab-profile.php
+++ hsin/ab-profile.php
@@ -25,14 +25,65 @@ $row = $pdo->query("SELECT * FROM member WHERE `sid`='$sid'")->fetch();
 ?>
 <?php include __DIR__ . '/parts-2/html-head-2.php' ?>
 <?php include __DIR__ . '/parts-2/navbar-3.php' ?>
-<br>
+<style>
+    body {
+        background-color: #69d0ff;
+        background-image: linear-gradient(0deg, #69d0ff 0%, #ffa4e9 100%);
+        background-position: 100%;
+        background-repeat: no-repeat;
+    }
+
+    .format {
+        font-size: 18px;
+        font-weight: 600;
+        font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    }
+
+    .format2 {
+        font-size: 18px;
+        font-weight: 600;
+        font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        color: #ebd367;
+    }
+
+    .pb-4 {
+        background-color: rgba(255, 255, 255, 0.6);
+        background-position: 100%;
+        background-repeat: no-repeat;
+    }
+
+    .btn-primary {
+        background-color: rgb(38, 106, 170);
+    }
+
+    .btn-primary:hover {
+        background-color: #fff;
+        color: rgb(38, 106, 170);
+    }
+
+    .btn-outline-primary {
+        border-color: rgb(38, 106, 170);
+        ;
+        color: rgb(38, 106, 170);
+    }
+
+    .btn-outline-primary:hover {
+        color: #fff;
+        background-color: rgb(38, 106, 170);
+    }
+
+    .card-2 {
+        margin-left: 20%;
+        /* margin-right: 50%; */
+    }
+</style>
 <div class="container">
     <div class="row">
         <div class="col">
             <div class="card-2 d-flex">
-                <div class="card" style="width: 20rem;font-size: 1.2rem;">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item" style="background-color: #f0f0f0;"><a href="ab-profile.php" style="text-decoration: none; color: #0d6efd">會員中心總覽 </a></li>
+                <div class="card" style="width: 20rem; font-size: 1.2rem;">
+                    <ul class="list-group list-group-flush format">
+                        <li class="list-group-item" style="background-color: #f0f0f0;"><a href="ab-profile.php" style="text-decoration: none; color: rgb(38, 106, 170);">會員中心總覽 </a></li>
                         <li class="list-group-item"><a href="ab-edit-profile.php" style="text-decoration: none; color: #212529">會員資料</a></li>
                         <li class="list-group-item">訂單總覽</li>
                         <li class="list-group-item">活動紀錄</li>
@@ -41,46 +92,48 @@ $row = $pdo->query("SELECT * FROM member WHERE `sid`='$sid'")->fetch();
                         <li class="list-group-item">我有問題</li>
                     </ul>
                 </div>
-                <div class="cards">
-                    <div class="card" style="width: 36rem;">
-                        <div class="card-body d-flex align-items-center">
-                            <div class="col-md-9 mb-md-0 p-md-4">
-                                <h5 class="card-title">歡迎回來，
-                                    <?php if (!empty($row['name'])) {
-                                        echo htmlentities($row['name']);
-                                    } else {
-                                        echo htmlentities($row['account']);
-                                    } ?></h5>
-                                <p class="card-text" style="font-size: 1rem; color: #707070;">請點擊下方按鈕瀏覽您的資料</p>
-
+                <div class="cards_out">
+                    <div class="cards">
+                        <div class="card" style="width: 100%; height: 50%">
+                            <div class="card-body d-flex align-items-center">
+                                <div class="card_pro"></div>
+                                <div class="col-md-9 mb-md-0 p-md-4">
+                                    <p class="card-title format">歡迎回來，<?php if (!empty($row['name'])) {
+                                                                            echo htmlentities($row['name']);
+                                                                        } else {
+                                                                            echo htmlentities($row['account']);
+                                                                        } ?></h5>
+                                    <p class="card-text" style="font-size: 1rem; color: #707070;">請點擊下方按鈕瀏覽您的資料</p>
+                                </div>
+                                <div class="col-md-9 mb-md-0 p-md-4 d-flex flex-row">
+                                    <i class="fa-solid fa-user-check card-img-top" style="font-size: 2.8rem; color: rgb(38, 106, 170);"></i>
+                                </div>
                             </div>
-                            <div class="col-md-9 mb-md-0 p-md-4 d-flex flex-row">
-                                <i class="fa-solid fa-user-check card-img-top" style="font-size: 2.8rem; color: #0d6efd;"></i>
-                            </div>
+                            <!-- <div class="revise_btn"> -->
+                            <a href="ab-edit-profile.php" class="btn btn-outline-primary stretched-link format">修改會員資料</a>
+                            <!-- </div> -->
                         </div>
-                        <a href="ab-edit-profile.php" class="btn btn-outline-primary stretched-link">修改會員資料</a>
-                    </div>
-                    <br>
-                    <div class="cards-2 d-flex justify-content-evenly align-items-center">
-                        <div class="card d-flex justify-content-evenly align-items-center" style="width: 18rem;">
-                            <div class="col-md-9 mb-md-0 p-md-4 d-flex flex-row justify-content-center">
-                                <i class="fa-solid fa-earth-asia" style="font-size: 2.5rem; color: #0d6efd;"></i>
+                        <div class="cards-2 d-flex justify-content-evenly align-items-center">
+                            <div class="card d-flex justify-content-evenly align-items-center" style="width: 18rem;">
+                                <div class="col-md-9 mb-md-0 p-md-4 d-flex flex-row justify-content-center">
+                                    <a href="#" i class="fa-solid fa-earth-asia" style="font-size: 2.5rem; color: rgb(38, 106, 170);"></a>
+                                </div>
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title format" style="text-align: center;">立即預訂下一份來生</h5>
+                                    <p class="card-text" style="font-size: 1rem; color: #707070;">點擊查看當前熱門轉生地點 ...</p>
+                                    <a href="#" class="btn btn-primary format">開始預約</a>
+                                </div>
                             </div>
-                            <div class="card-body d-flex flex-column">
-                                <h5 class="card-title" style="text-align: center;">立即預訂下一份來生</h5>
-                                <p class="card-text" style="font-size: 1rem; color: #707070;">點擊查看當前熱門轉生地點 ...</p>
-                                <a href="#" class="btn btn-primary">開始預約</a>
-                            </div>
-                        </div>
-                        <div class="card d-flex justify-content-evenly align-items-center" style="width: 18rem;">
-                            <div class="col-md-9 mb-md-0 p-md-4 d-flex flex-row justify-content-center">
-                                <i class="fa-solid fa-wand-magic-sparkles" style="font-size: 2.5rem; color: #0d6efd;"></i>
-                            </div>
-                            <div class="card-body d-flex flex-column">
-                                <h5 class="card-title" style="text-align: center;">打造您的下一份來生</h5>
-                                </h5>
-                                <p class="card-text" style="font-size: 1rem; color: #707070;">即刻為您的來生打造全新樣貌 ...</p>
-                                <a href="#" class="btn btn-primary">開始創建</a>
+                            <div class="card d-flex justify-content-evenly align-items-center" style="width: 18rem;">
+                                <div class="col-md-9 mb-md-0 p-md-4 d-flex flex-row justify-content-center">
+                                    <a href="#" i class="fa-solid fa-wand-magic-sparkles" style="font-size: 2.5rem; color: rgb(38, 106, 170);"></a>
+                                </div>
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title format" style="text-align: center;">打造您的下一份來生</h5>
+                                    </h5>
+                                    <p class="card-text" style="font-size: 1rem; color: #707070;">即刻為您的來生打造全新樣貌 ...</p>
+                                    <a href="#" class="btn btn-primary format">開始創建</a>
+                                </div>
                             </div>
                         </div>
                     </div>
