180a7b50ca68d9b4561af7ec996e6234eadb1b9a
Dandelion64 <coolpillow0308@gmail.com>
2022-06-10 21:45:23 +0800

    count number of acts in cart

---
+++ proj/count-cart.php
@@ -0,0 +1,13 @@
+<?php require __DIR__ . './parts/connect_db.php' ?>
+
+<?php 
+
+if (isset($_SESSION['cart'])) {
+    $output = count($_SESSION['cart'], 0);
+} else {
+    $output = 0; 
+}
+
+echo $output;
+
+?>
