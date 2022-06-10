2f926aaa0ff1e43e578ce36260c7c0a949971056
hsin007 <a2827234@gmail.com>
2022-06-10 11:57:02 +0800

    update 100 accounts on sql file

--- hsin/ab-list-api.php
+++ hsin/ab-list-api.php
@@ -25,7 +25,7 @@ if ($totalRows > 0) {
         exit;
     }
 
-    $sql = sprintf("SELECT * FROM member ORDER BY sid DESC LIMIT %s, %s", ($page - 1) * $perPage, $perPage);
+    $sql = sprintf("SELECT * FROM member ORDER BY sid ASC LIMIT %s, %s", ($page - 1) * $perPage, $perPage);
     $rows = $pdo->query($sql)->fetchAll();
 }
 
