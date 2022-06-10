99deab5b667791516104f5e391a8b040885979b0
yun lee <christinalee22l4@gmail.com>
2022-06-10 10:34:51 +0800

    record系列 是後台 尚未完成 mainpage 的連結待修正 test_page 修正

--- yun/test/mainpage.php
+++ yun/test/mainpage.php
@@ -8,36 +8,50 @@ $title = 'MainPage';
 
 <style>
   .card {
-    margin: auto;
-    margin-top: 100px;
+    margin: 100px auto auto;
+   
 
   }
 
-  .testDone {
-    display: none;
-  }
+
 </style>
 
 <?php
-
+//先做一筆假資料測試用
+$_SESSION['member']['sid'] = 100;
 $_SESSION['member']['account'] = 'meowmeow';
 $_SESSION['member']['name'] = '卯咪';
-$_SESSION['member']['testscore'] = '10000';
 
+//若要重整假資料時可以先做清除
 // if (isset($_SESSION['member']['account'])){
-//   unset($_SESSION['member']['name']);
-//   unset($_SESSION['member']['testscore']);
+//   unset($_SESSION['member']['sid']);
 //   unset($_SESSION['member']['account']);
+//   unset($_SESSION['member']['name']);
 
 // }
+
+
+
 ?>
 <?php
 if (!empty($_SESSION['member']['account'])) {
+  
+  $sid = $_SESSION['member']['sid'];
+  $t_sql = "SELECT `test_score` FROM `good_deed_test_record` WHERE `member_sid`= $sid";
+  $row = $pdo->query($t_sql)->fetch();
+  $score =  $row['test_score'];
+  
+  $account =  $_SESSION['member']['account'];
+  $name = $_SESSION['member']['name'];
+  
+  // //for test
+  // $score = 100;
+
 ?>
   <?php
-  if (!empty($_SESSION['member']['testscore'])) { ?>
-    <!-- //這部分要看余欣的欄位怎麼設定
-      //有分數的話呈現分數 -->
+  if (!empty($score)) { ?>
+    <!-- 這部分要看余欣的欄位怎麼設定
+      有分數的話呈現分數 -->
     <div class="container testDone">
       <div class="col">
         <!-- <div class="card" style="width: 18rem;"> -->
@@ -47,7 +61,7 @@ if (!empty($_SESSION['member']['account'])) {
           <div class="card-body">
             <h5 class="card-title">
               Hi <?= $_SESSION['member']['name'] ?> ！
-              你的陰德值為:<?= $_SESSION['member']['testscore'] ?>
+              你的陰德值為:<?= $score ?? 0 ?>
             </h5>
             <p class="card-text">
               覺得陰德值太少嗎?
@@ -57,9 +71,11 @@ if (!empty($_SESSION['member']['account'])) {
               慈善捐款
             </a>
             <a href="#" class="btn btn-primary">
+              <!-- 這邊可以放容瑄的連結 -->
+            
               參與慈善活動
             </a>
-            <a href="#" class="btn btn-primary">
+            <a href="./gamespage.php" class="btn btn-primary">
               遊玩慈善遊戲
             </a>
           </div>
@@ -68,7 +84,7 @@ if (!empty($_SESSION['member']['account'])) {
 
     </div>
   <?php } else { ?>
-    <!-- //沒有分數的話就做測驗 -->
+    <!-- 沒有分數的話就做測驗 -->
     <div class="container">
       <div class="col">
         <!-- <div class="card" style="width: 18rem;"> -->
@@ -76,13 +92,13 @@ if (!empty($_SESSION['member']['account'])) {
           <!-- <img src="..." class="card-img-top" alt="..."> -->
           <div class="card-body">
             <h5 class="card-title">
-              Hi <?= $_SESSION['member']['name'] ?> ！
+              Hi <?= $_SESSION['member']['name']? $_SESSION['member']['name'] : "" ?> ！
             </h5>
             <p class="card-text">
               想知道你有多少陰德值嗎?
               來測測看吧!
             </p>
-            <a href="#" class="btn btn-primary">
+            <a href="./test_page.php" class="btn btn-primary">
               陰德值測驗Go!
             </a>
           </div>
@@ -92,15 +108,9 @@ if (!empty($_SESSION['member']['account'])) {
   <?php
   }
 } else {
-  // 跳轉到登入頁面
-  $error_msg = '你尚未登入！';
-  ?>
-  <script>
-    setTimeout(() => {
-      location.href = '#';
-    }, 2000);
-  </script>
-<?php
+  // 跳轉到余欣的登入頁面
+  
+//  header(location:login.php);
 }
 
 ?>
--- yun/test/test_page-api.php
+++ yun/test/test_page-api.php
@@ -11,25 +11,9 @@ $output = [
     'error' => ''
 ];
 
-$sid = isset($_POST['sid']) ? intval($_POST['sid']) : 0;
-// if (empty($sid) or empty($_POST['name'])) {
-//     $output['error'] = '沒有登入';
-//     $output['code'] = 400;
-//     echo json_encode($output, JSON_UNESCAPED_UNICODE);
-//     exit;
-// }
-// $account = $_POST['account'];
-// $password = $_POST['password'];
-// $name = $_POST['name'];
-// $birth = $_POST['birth'];
-// $death = empty($_POST['death']) ? NULL : $_POST['death'];
+//使用sid做比對 在對應位置填入資料
+$sid = isset($_SESSION['member']['sid']) ? intval($_SESSION['member']['sid']) : 0;
 
-
-// $q1 = empty($_POST['Q1']) ? NULL : $_POST['Q1'];
-// $q2 = empty($_POST['Q2']) ? NULL : $_POST['Q2'];
-// $q3 = empty($_POST['Q3']) ? NULL : $_POST['Q3'];
-// $q4 = empty($_POST['Q4']) ? NULL : $_POST['Q4'];
-// $q5 = empty($_POST['Q5']) ? NULL : $_POST['Q5'];
 
 $q1 = intval($_POST['Q1']);
 $q2 = intval($_POST['Q2']);
@@ -40,19 +24,18 @@ $q5 = intval($_POST['Q5']);
 
 // TODO: 其他欄位檢查
 
-$score = $q1 * 1 + $q2 * 2 + $q3 * 3 + $q4 * 4 + $q5 * 5; 
+$score = $q1 * rand(50, 99) + $q2 * rand(50, 99) + $q3 * rand(50, 99) + $q4 * rand(50, 99) + $q5 * rand(50, 99);
 
-$test_sql = 
+$test_sql =
     "SELECT COUNT(*) 
     FROM `good_deed_test_record`
-    WHERE `sid`=$sid"
-;
+    WHERE `sid`=$sid";
 
 $rowNumber = $pdo->query($test_sql)->fetch(PDO::FETCH_NUM)[0];
 
 if ($rowNumber == 0) {
-    $sql = 
-    "INSERT INTO `good_deed_test_record`(
+    $sql =
+        "INSERT INTO `good_deed_test_record`(
         `test_Q1`, `test_Q2`, `test_Q3`, 
         `test_Q4`, `test_Q5`, `test_score`
     ) VALUES (
@@ -81,11 +64,10 @@ if ($rowNumber == 0) {
     echo json_encode($output, JSON_UNESCAPED_UNICODE);
     exit;
 } else if ($rowNumber == 1) {
-    $sql = 
+    $sql =
         "UPDATE `good_deed_test_record` 
         SET `test_Q1`=?,`test_Q2`=?,`test_Q3`=?,`test_Q4`=?,`test_Q5`=?,`test_score`=? 
-        WHERE `sid`=$sid"
-    ;
+        WHERE `sid`=$sid";
 
     $stmt = $pdo->prepare($sql);
 
@@ -101,7 +83,7 @@ if ($rowNumber == 0) {
     if ($stmt->rowCount() == 1) {
         $output['success'] = true;
         // 最近新增資料的 primery key
-    
+
     } else {
         $output['error'] = '陰德值計算失敗';
     }
