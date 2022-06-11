08368f6c9b2a10aa3fec768b38b9a68e00f1ce0f
Mou <akimouin@gmail.com>
2022-06-11 23:53:32 +0800

    倒序撈取我的衣櫥

--- proj/avatar-getshowcase-api.php
+++ proj/avatar-getshowcase-api.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 
 $mid = $_POST['mid'];
 
-$avatars = $pdo->query("SELECT * FROM `showcase` WHERE `member_sid` = $mid")->fetchAll();
+$avatars = $pdo->query("SELECT * FROM `showcase` WHERE `member_sid` = $mid ORDER BY `avatar_created_at` DESC")->fetchAll();
 
 
 echo json_encode($avatars, JSON_UNESCAPED_UNICODE);
