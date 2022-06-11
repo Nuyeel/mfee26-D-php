a2ec757c1b5571438197c3692a4c20d7ecae2bc8
ChangRongXuan <as840922qq@gmail.com>
2022-06-12 00:14:55 +0800

    會員分頁 CSS 統合

--- proj/ab-event.php
+++ proj/ab-event.php
@@ -44,36 +44,111 @@ $event = $pdo->query($IDevent)->fetchAll();
 <?php include __DIR__ . "./parts/navbar.php" ?>
 
 <style>
-    .form-control.red {
+    /* .form-control.red {
         border: 1px solid red;
     }
 
     .form-text.red {
         color: red;
+    } */
+
+
+    body {
+        background-color: #69d0ff;
+        background-image: linear-gradient(0deg, #69d0ff 0%, #ffa4e9 100%);
+        background-position: 100%;
+        background-repeat: no-repeat;
+        height: 100vh;
     }
+
+    .format {
+        font-size: 18px;
+        font-weight: 600;
+        font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    }
+
+    .format2 {
+        font-size: 18px;
+        font-weight: 600;
+        font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        color: #ebd367;
+    }
+
+    .pb-4 {
+        background-color: rgba(255, 255, 255, 0.6);
+        background-position: 100%;
+        background-repeat: no-repeat;
+    }
+
+    .btn-primary {
+        background-color: rgb(38, 106, 170);
+    }
+
+    .btn-primary:hover {
+        background-color: #fff;
+        color: rgb(38, 106, 170);
+    }
+
+    .btn-outline-primary {
+        border-color: rgb(38, 106, 170);
+        ;
+        color: rgb(38, 106, 170);
+    }
+
+    .btn-outline-primary:hover {
+        color: #fff;
+        background-color: rgb(38, 106, 170);
+    }
+
+    .row {
+        margin-left: 15%;
+        /* margin-right: 50%; */
+    }
+
+    .icon {
+        text-decoration: none;
+        font-size: 2.6rem;
+        transition: .3s;
+        color: rgb(38, 106, 170);
+        color: rgb(38, 106, 170);
+    }
+
+    .icon:hover {
+        color: #ebd367;
+    }
+
+    .format3 {
+        font-size: 20px;
+        font-weight: 600;
+        font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        color: rgb(38, 106, 170);
+    }
+
+
 </style>
-<br>
+
 <div class="container">
     <div class="row">
         <div class="col">
             <div class="card_2 d-flex">
                 <div class="card" style="width: 20rem;font-size: 1.2rem;">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><a href="ab-profile.php" style="text-decoration: none; color: #212529">會員中心總覽 </a></li>
-                        <li class="list-group-item"><a href="ab-edit-profile.php" style="text-decoration: none; color: #212529" >會員資料</a></a></li>
+                    <ul class="list-group list-group-flush format">
+                        <li class="list-group-item" ><a href="ab-profile.php" style="text-decoration: none; color: #212529">會員中心總覽 </a></li>
+                        <li class="list-group-item"><a href="ab-edit-profile.php" style="text-decoration: none; color: #212529">會員資料</a></li>
                         <li class="list-group-item"><a href="ab-place.php"style="text-decoration: none; color: #212529">訂單總覽</a></li>
-                        <li class="list-group-item" style="background-color: #f0f0f0;"><a href="ab-event.php" style="text-decoration: none; color: #0d6efd">活動紀錄</a></li>
+                        <li class="list-group-item" style="background-color: #f0f0f0;"><a href="ab-event.php" style="text-decoration: none; color: rgb(38, 106, 170);">活動紀錄</a></li>
                         <li class="list-group-item"><a href="ab-showcase.php" style="text-decoration: none; color: #212529">衣櫥間</a></li>
+                        <!--
                         <li class="list-group-item">常見問題</li>
-                        <li class="list-group-item">我有問題</li>
+                        <li class="list-group-item">我有問題</li> -->
                     </ul>
                 </div>
 
 
                 <!-- 放各類別要呈現的內容（選項列的右邊那塊） -->
-                <div class="card" style="width: 60rem;">
+                <div class="card" style="max-width:43rem">
                     <div class="card-body" >
-                        <h4 class="card-title" style="font-size:18px; font-weight:bold">活動報名紀錄</h4>
+                        <h4 class="card-title format3 " >活動紀錄</h4>
                         <br>
 
                         <!-- 這邊導入會員名稱 -->
@@ -88,11 +163,11 @@ $event = $pdo->query($IDevent)->fetchAll();
 
                 <!-- 卡片式 -->
 
-                <div class="container d-flex align-content-start flex-wrap " style="width:80%;">
+                <div class="container w-100 d-flex align-content-start; flex-wrap" style="padding:0px 5px;margin:0px 5px;">
 
                 <?php foreach ($event as $e) : ?>
 
-                    <div class="card m-2 mt-3 card-f" style="width: 18rem; border-radius:10px; cursor:pointer">
+                    <div class="card m-2 mt-3 card-f" style="width: 18rem; border-radius:10px; cursor:pointer;min-width:18rem">
 
                         <a href="event-detail.php"></a>
                         <div class="d-flex justify-content-between" style="padding:10px 15px">
--- proj/ab-showcase.php
+++ proj/ab-showcase.php
@@ -60,28 +60,100 @@ $show = $pdo->query($showcase)->fetchAll();
         margin: auto;
         text-align: center;
     }
+
+    body {
+        background-color: #69d0ff;
+        background-image: linear-gradient(0deg, #69d0ff 0%, #ffa4e9 100%);
+        background-position: 100%;
+        background-repeat: no-repeat;
+        height: 100vh;
+    }
+
+    .format {
+        font-size: 18px;
+        font-weight: 600;
+        font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    }
+
+    .format2 {
+        font-size: 18px;
+        font-weight: 600;
+        font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        color: #ebd367;
+    }
+
+    .pb-4 {
+        background-color: rgba(255, 255, 255, 0.6);
+        background-position: 100%;
+        background-repeat: no-repeat;
+    }
+
+    .btn-primary {
+        background-color: rgb(38, 106, 170);
+    }
+
+    .btn-primary:hover {
+        background-color: #fff;
+        color: rgb(38, 106, 170);
+    }
+
+    .btn-outline-primary {
+        border-color: rgb(38, 106, 170);
+        ;
+        color: rgb(38, 106, 170);
+    }
+
+    .btn-outline-primary:hover {
+        color: #fff;
+        background-color: rgb(38, 106, 170);
+    }
+
+    .row {
+        margin-left: 15%;
+        /* margin-right: 50%; */
+    }
+
+    .icon {
+        text-decoration: none;
+        font-size: 2.6rem;
+        transition: .3s;
+        color: rgb(38, 106, 170);
+        color: rgb(38, 106, 170);
+    }
+
+    .icon:hover {
+        color: #ebd367;
+    }
+
+    .format3 {
+        font-size: 20px;
+        font-weight: 600;
+        font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        color: rgb(38, 106, 170);
+    }
 </style>
-<br>
+
 <div class="container">
     <div class="row">
         <div class="col">
             <div class="card_2 d-flex">
                 <div class="card" style="width: 20rem;font-size: 1.2rem;">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><a href="ab-profile.php" style="text-decoration: none; color: #212529">會員中心總覽 </a></li>
-                        <li class="list-group-item"><a href="ab-edit-profile.php" style="text-decoration: none; color: #212529">會員資料</a></a></li>
-                        <li class="list-group-item"><a href="ab-place.php" style="text-decoration: none; color: #212529">訂單總覽</a></li>
+                    <ul class="list-group list-group-flush format">
+                        <li class="list-group-item" ><a href="ab-profile.php" style="text-decoration: none; color: #212529">會員中心總覽 </a></li>
+                        <li class="list-group-item"><a href="ab-edit-profile.php" style="text-decoration: none; color: #212529">會員資料</a></li>
+                        <li class="list-group-item"><a href="ab-place.php"style="text-decoration: none; color: #212529">訂單總覽</a></li>
                         <li class="list-group-item"><a href="ab-event.php" style="text-decoration: none; color: #212529">活動紀錄</a></li>
-                        <li class="list-group-item" style="background-color: #f0f0f0;"><a href="ab-event.php" style="text-decoration: none; color: #0d6efd">衣櫥間</a></li>
+                        <li class="list-group-item" style="background-color: #f0f0f0;"><a href="ab-showcase.php" style="text-decoration: none; color: rgb(38, 106, 170);">衣櫥間 </a></li>
+                        <!--
                         <li class="list-group-item">常見問題</li>
-                        <li class="list-group-item">我有問題</li>
+                        <li class="list-group-item">我有問題</li> -->
                     </ul>
                 </div>
 
                 <!-- 這一段放各類別要呈現的內容（選項列的右邊那塊） -->
-                <div class="card" style="width: 60rem; padding-bottom:20px">
+                <div class="card" style="min-width:30rem;max-width:52rem; padding-bottom:20px">
                     <div class="card-body">
-                        <h4 class="card-title" style="font-size:18px; font-weight:bold">訂單總覽</h4>
+                        <h4 class="card-title format3" >衣櫥間</h4>
                         <br>
 
                         <!-- 這邊導入會員名稱 -->
@@ -96,13 +168,13 @@ $show = $pdo->query($showcase)->fetchAll();
 
 
 
-                    <div class="container-fluid d-flex align-content-start flex-wrap w-75">
+                    <div class="container-fluid d-flex align-content-start flex-wrap " style="padding:0px 5px;margin:0px 5px;">
                         <form action="" id="form1" name="form1" onsubmit="return false;" style="display: none;">
                             <input class="form-check-input" type="text" name="mid" value="<?php echo $_SESSION['member']['sid']; ?>" checked>
                         </form>
 
                         <!-- 這邊放要展示的資料，用php foreach下去跑， -->
-                        <div class="row" id="showcase">
+                        <div class="row " id="showcase" style="margin-left:5px;margin-right:5px; ">
                         </div>
 
                     </div>
